15b77d69cc7f8ffa137300234eeacd13b9cafa4d
Masked Phrogg <55362644+PhroggDev@users.noreply.github.com>
2022-06-25 18:51:31 -0400

    initial commit add mkpassword.py

---
+++ mkpassword.py
@@ -0,0 +1,9 @@
+import string
+import secrets
+
+
+length = 15
+# Choose wide set of characters, but consider what your system can handle
+alphabet = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(alphabet) for i in range(length))
+print(password)

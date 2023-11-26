94d7baeaf95a55396a81f873d371462333433590
PhroggDev <55362644+PhroggDev@users.noreply.github.com>
2023-11-26 10:47:23 -0500

    Update mkpassword.py; edit print statement

--- mkpassword.py
+++ mkpassword.py
@@ -6,4 +6,4 @@ length = 15
 # Choose wide set of characters, but consider what your system can handle
 alphabet = string.ascii_letters + string.digits + string.punctuation
 password = ''.join(secrets.choice(alphabet) for i in range(length))
-print(password)
+print(f"You new password is: {password}")
